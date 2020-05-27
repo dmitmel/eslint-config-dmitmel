0b68ecc53dc877c66dd5bd52f5d0925bbdecdcf6
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-05-27 09:57:19 +0300

    remove optional dependencies

--- presets/core.js
+++ presets/core.js
@@ -1,12 +1,3 @@
-function moduleExists(id) {
-  try {
-    require.resolve(id);
-    return true;
-  } catch (_e) {
-    return false;
-  }
-}
-
 module.exports = {
   extends: [
     'eslint:recommended',
@@ -19,7 +10,6 @@ module.exports = {
     es2017: true,
   },
 
-  parser: moduleExists('babel-eslint') ? 'babel-eslint' : null,
   parserOptions: {
     ecmaVersion: 8,
   },
